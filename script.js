73b56198548c03c2eaa000bc8e28a8491bb3dcaa
Tancredi <tancredi.dangelo.22@gmail.com>
2026-03-18 15:37:47 +0100

    updated a function

--- script.js
+++ script.js
@@ -5,26 +5,30 @@ const taskList = document.getElementById("taskList")
 
 const addTask = function(e) {
     e.preventDefault()
-    let inputValue = input.value
-    let taskCard = document.createElement("li")
+    if (input.value !== "") {
+        let inputValue = input.value
+        let taskCard = document.createElement("li")
 
-    let taskText = document.createElement("p")
-    taskText.innerText = inputValue
-    
-    let checkBox = document.createElement("button")
-    checkBox.setAttribute("id", "taskDone")
-    checkBox.innerText = "✔️"
-    checkBox.addEventListener("click", crossTask)
+        let taskText = document.createElement("p")
+        taskText.innerText = inputValue
+        
+        let checkBox = document.createElement("button")
+        checkBox.setAttribute("id", "taskDone")
+        checkBox.innerText = "✔️"
+        checkBox.addEventListener("click", crossTask)
 
-    let deleteButton = document.createElement("button")
-    deleteButton.setAttribute("id", "deleteButton")
-    deleteButton.innerText = "❌"
-    deleteButton.addEventListener("click", removeTask)
-    
-    taskCard.append(taskText, checkBox, deleteButton)
-    taskList.appendChild(taskCard)
+        let deleteButton = document.createElement("button")
+        deleteButton.setAttribute("id", "deleteButton")
+        deleteButton.innerText = "❌"
+        deleteButton.addEventListener("click", removeTask)
+        
+        taskCard.append(taskText, checkBox, deleteButton)
+        taskList.appendChild(taskCard)
 
-    input.value = ""
+        input.value = ""
+    }   else {
+        alert("type something")
+    }
 }
 
 const crossTask = function(e) {
