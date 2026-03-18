e0f26c9a8c9d8e200f7ce964722e99b87c3904a3
Tancredi <tancredi.dangelo.22@gmail.com>
2026-03-18 15:31:24 +0100

    exercise completed

---
+++ script.js
@@ -0,0 +1,39 @@
+const form = document.getElementById("taskForm")
+const button = document.getElementById("submitButton")
+const input = document.getElementById("taskInput")
+const taskList = document.getElementById("taskList")
+
+const addTask = function(e) {
+    e.preventDefault()
+    let inputValue = input.value
+    let taskCard = document.createElement("li")
+
+    let taskText = document.createElement("p")
+    taskText.innerText = inputValue
+    
+    let checkBox = document.createElement("button")
+    checkBox.setAttribute("id", "taskDone")
+    checkBox.innerText = "✔️"
+    checkBox.addEventListener("click", crossTask)
+
+    let deleteButton = document.createElement("button")
+    deleteButton.setAttribute("id", "deleteButton")
+    deleteButton.innerText = "❌"
+    deleteButton.addEventListener("click", removeTask)
+    
+    taskCard.append(taskText, checkBox, deleteButton)
+    taskList.appendChild(taskCard)
+
+    input.value = ""
+}
+
+const crossTask = function(e) {
+    let taskToCross = e.target.parentElement.querySelector("p")
+    taskToCross.setAttribute("style", "text-decoration: line-through;")
+}
+
+const removeTask = function(e) {
+    e.target.parentElement.remove()
+}
+
+button.addEventListener("click", addTask)
